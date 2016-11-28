bb56c3859c699c0ffc0bf1cc80bc8d85b304a199
Cameron Moeller <cameron.moeller@gmail.com>
2016-11-28 16:26:04 -0500

    10day formatting worked on

--- tenday.py
+++ tenday.py
@@ -7,5 +7,16 @@ class Tenday():
     def ten(self):
         url = "http://api.wunderground.com/api/a40667feb0e90ab4/forecast10day/q/" + input("zipcode: ") + ".json"
         r = requests.get(url)
-        results = r.json()
-        print(json.dumps(r.json(), indent=4))
+        i = 0
+        while i < 10:
+            low_f = r.json()['forecast']['simpleforecast']['forecastday'][i]['low']['fahrenheit']
+            high_f = r.json()['forecast']['simpleforecast']['forecastday'][i]['high']['fahrenheit']
+            conditions = r.json()['forecast']['simpleforecast']['forecastday'][i]['conditions']
+            maxwind = r.json()['forecast']['simpleforecast']['forecastday'][i]['maxwind']['mph']
+            dayweek = r.json()['forecast']['simpleforecast']['forecastday'][i]['date']['weekday']
+            print("\n\n{}\nLow: {} High: {}\nConditions: {}\nMaxWind: {}\n\n".format(dayweek, low_f, high_f, conditions, maxwind))
+            i+=1
+
+
+
+        #print(json.dumps(results, indent=4))
